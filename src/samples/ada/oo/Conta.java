c1a9176849a98be8c5822b7d5ed109613af39ccf
Matheus Sousa <alvessousam6@gmail.com>
2026-02-19 22:23:58 -0300

    Mudanças nos itens para demostração de composição e associação entre objetos.

--- src/samples/ada/oo/Conta.java
+++ src/samples/ada/oo/Conta.java
@@ -1,21 +1,47 @@
 package samples.ada.oo;
 
 public class Conta {
-	int numero;
-	double saldo;
-	Pessoa pessoa;
+	private int numero;
+	private double saldo;
+	private Pessoa pessoa;
+	Pessoa[] dependentes;
+
 	public Conta(int numero, double saldoParam, Pessoa pessoa){
 		this.numero = numero;
 		this.saldo = saldoParam;
 		this.pessoa = pessoa;
 	}
+
 	public Conta(int numero, Pessoa pessoa){
 		this.numero = numero;
 		this.saldo = 0.0;
 		this.pessoa = pessoa;
 	}
 
-	double sacar(double valor){return 0;}
-	double depositar(double valor){return 0;}
+	public double sacar(double valor){
+			validaSaldo(valor);
+			saldo -= valor;
+			return saldo;
+	}
+
+	private void validaSaldo(double valor){
+		if(saldo<=valor){
+			throw new RuntimeException("Saldo insuficiente");
+		}
+	}
+
+	double depositar(double valor){
+		this.numero = 345;
+		saldo +=valor;
+		return saldo;
+	}
 	double realizarEmprestimo(double valor){return 0;}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
 }
--- src/samples/ada/oo/NossoEstudoOO.java
+++ src/samples/ada/oo/NossoEstudoOO.java
@@ -1,6 +1,6 @@
 package samples.ada.oo;
 
-public class NossEstudoOO {
+public class NossoEstudoOO {
 	public static void main(String[] args) {
 		Pessoa pessoa = new Pessoa("Claudia");
 		Conta exemploConta = new Conta(123, pessoa);
